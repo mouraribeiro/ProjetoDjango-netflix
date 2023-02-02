8b91690cb577acdcb705bed07e372fcb4d37e60e
Rayanne <mouraribeiro11@gmail.com>
2023-02-02 20:27:09 -0300

    detalhes filmes

--- filme/views.py
+++ filme/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Filme
+from django.views.generic import TemplateView, ListView, DetailView
+
 
 # Create your views here.
-def homepage(request):
-    return render(request, 'homepage.html')
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+
+
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Filme
+
+class Detalhesfilme(DetailView):
+    template_name = "detalhesfilme.html"
+    model = Filme
